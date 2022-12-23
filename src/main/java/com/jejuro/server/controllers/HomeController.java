48fbdb7b2761d1c75b787023c02699e1baea3f0e
jungwoo <114479384+clwhale@users.noreply.github.com>
2022-12-23 17:03:59 +0900

    Update HomeController.java

--- src/main/java/com/jejuro/server/controllers/HomeController.java
+++ src/main/java/com/jejuro/server/controllers/HomeController.java
@@ -29,8 +29,8 @@ public class HomeController {
     public String signup(){
         return "html/sign-up/sign-up";
     }
-    @GetMapping("findpasswd")
-    public String findpasswd(){
-        return "html/findpasswd/findpasswd";
+    @GetMapping("password")
+    public String password(){
+        return "html/findpasswd/password";
     }
 }
