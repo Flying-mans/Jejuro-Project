3a3aa97d9e54b70722811e6c7d614332707d2e8c
thislast <97195201+thislast@users.noreply.github.com>
2022-12-28 14:54:32 +0900

    post asyn
    게시판 비동기처리 onclick event
    
    api 추가
    asyn js 추가가
    asyn에 따른 html 수정
    
    controller 수정

---
+++ src/main/java/com/jejuro/server/api/PostApi.java
@@ -0,0 +1,43 @@
+package com.jejuro.server.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jejuro.server.entity.Post;
+import com.jejuro.server.service.PostService;
+
+@RestController
+@RequestMapping("/api")
+public class PostApi {
+
+	@Autowired
+	private PostService service;
+	
+	@GetMapping("posts/{page}")
+	public List<Post> getPost(
+			@PathVariable("page") int page) {
+		
+		List<Post> post = service.getPost(page);
+		
+
+		System.out.println(page);
+		return post;
+	}
+	
+//	@GetMapping("post/{id}")
+//	public String detail(
+//			@PathVariable("id")int id,
+//			Model model) {
+//
+//		Post post = service.get(id);
+//		model.addAttribute("post",post);
+//
+//		return "html/post/mod";
+//	}
+}
