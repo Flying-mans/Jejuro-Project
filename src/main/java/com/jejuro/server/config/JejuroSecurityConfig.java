b35d1b535af024190d2c02f5301bd47e9054f13b
Jungwoo <cloudwhale99@gmail.com>
2023-01-02 18:18:25 +0900

    Add: search page authority add member
    search 페이지 권한 멤버도 가능하게 추가했습니다.

--- src/main/java/com/jejuro/server/config/JejuroSecurityConfig.java
+++ src/main/java/com/jejuro/server/config/JejuroSecurityConfig.java
@@ -13,43 +13,44 @@ import com.jejuro.server.auth.service.JejuroUserDetailsService;
 @Configuration
 public class JejuroSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests(authorize -> authorize // 회원 권한 테스트
-                        .requestMatchers("/search/**").hasAnyRole("ADMIN")
-                        .requestMatchers("/post/**").hasAnyRole("MEMBER", "ADMIN")
-                        .anyRequest().permitAll())
+                http
+                                .csrf()
+                                .disable()
+                                .authorizeHttpRequests(authorize -> authorize // 회원 권한 테스트
+                                                .requestMatchers("/search/**").hasAnyRole("MEMBER", "ADMIN")
+                                                .requestMatchers("/post/**").hasAnyRole("MEMBER", "ADMIN")
+                                                .anyRequest().permitAll())
+                                
+                                .exceptionHandling(exp -> exp
+                                                .accessDeniedPage("/denied")) // 인가 실패시 처리
 
-                .formLogin(form -> form
-                        .loginPage("/member/login")
-                        .defaultSuccessUrl("/index"))
+                                .formLogin(form -> form
+                                                .loginPage("/member/login")
+                                                .defaultSuccessUrl("/index"))
 
-                .exceptionHandling(exp -> exp
-                        .accessDeniedPage("/denied")) // 인가 실패시 처리
+                                .logout(form -> form
+                                                .logoutUrl("/member/logout")
+                                                .logoutSuccessUrl("/index"))
 
-                .logout(form -> form
-                        .logoutUrl("/member/logout")
-                        .logoutSuccessUrl("/index"))
-                .rememberMe(form -> form
-                        .key("uniqueAndSecret") // 인증을 위해 발행되는 토큰을 구별하는 키 이름
-                        .rememberMeParameter("remember-me") // 로그인 페이지 html 파라미터 이름
-                        .tokenValiditySeconds(60 * 60 * 24)) // 토큰 유효시간(s) 디폴트는 2주
+                                .rememberMe(form -> form
+                                                .key("uniqueAndSecret") // 인증을 위해 발행되는 토큰을 구별하는 키 이름
+                                                .rememberMeParameter("remember-me") // 로그인 페이지 html 파라미터 이름
+                                                .tokenValiditySeconds(60 * 60 * 24)) // 토큰 유효시간(s) 디폴트는 2주
                 ;
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    };
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        };
 
-    @Bean
-    public UserDetailsService jejuroUserDetailsService() {
-        return new JejuroUserDetailsService();
-    }
+        @Bean
+        public UserDetailsService jejuroUserDetailsService() {
+                return new JejuroUserDetailsService();
+        }
 }
