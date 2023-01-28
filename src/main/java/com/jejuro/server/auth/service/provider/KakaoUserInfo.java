8690227f6c3f6c922c1a6f35519313368b37915c
Jungwoo <cloudwhale99@gmail.com>
2023-01-28 10:38:36 +0900

    Feat: OAuth2UserInfo and snsUserInfo
    OAuth2 제공자들 마다 응답해주는 속성값이 달라서 공통으로 OAuth2UserInfo interface를 만들어주고 각각의 snsUserInfo가 implements을 해서 사용한다.

---
+++ src/main/java/com/jejuro/server/auth/service/provider/KakaoUserInfo.java
@@ -0,0 +1,31 @@
+package com.jejuro.server.auth.service.provider;
+
+import java.util.Map;
+
+public class KakaoUserInfo implements OAuth2UserInfo {
+    private Map<String, Object> attributes;
+
+    public KakaoUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("nickname");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+}
